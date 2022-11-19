47e87898bc14cee2d8ab3bbe7fb82208924b8224
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-11-19 15:07:32 +0200

    nested routes

--- crwn-clothing/src/routes/shop/shop.component.jsx
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,25 +1,15 @@
-import { CategoriesContext } from "../../contexts/categories.contex";
-import { Fragment, useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 import "./shop.styles.scss";
 
+
+
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+<Routes>
+    <Route index element={<CategoriesPreview/>}/>
+</Routes>
   )
 }
 export default Shop;
