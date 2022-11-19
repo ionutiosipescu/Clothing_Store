40f5aeb3354bb18f55c58c68434e6ab83c3ac2b9
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-11-19 13:22:44 +0200

    initial commit

---
+++ crwn-clothing/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
+
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
