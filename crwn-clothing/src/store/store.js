43c28bf77566b646823f29173186334fcc7f4fe8
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-11-24 15:19:55 +0200

    redux thunk for async functions

--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -2,6 +2,7 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
@@ -20,9 +21,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 // After
 // vedem state-ul in consola + isi da seama daca esti in developer mode sau production
 // pentru a activa acest logger
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
+
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 
 // verifica in ce envaroment esti si iti permite sa folosesti redux extension din crome
 // optional
@@ -41,3 +44,8 @@ export const store = createStore(
 );
 
 export const persistor = persistStore(store);
+
+// THUNK este folosit pentru functiile async
+// aduce modificari in mai multe fisiere dar este foarte asemanator cu procesele
+// care sunt related cu async functions, adica avem chestiile legate de Promiseuri
+// start, succes, failed, async, await, try, catch, consume
