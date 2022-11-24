7eb2bd2386a5c3c2eb4f8ae4c92caf37db35f22a
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-11-24 11:09:12 +0200

    update

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -77,15 +77,10 @@ export const addCollectionAndDocuments = async (
 
 export const getCategoriesAndDocuments = async () => {
   const collectionRef = collection(db, "categories");
-  const q = query(collectionRef)
+  const q = query(collectionRef);
 
-  const querySnapshot = await getDocs(q)
-  const categoryMap =  querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const {title, items} = docSnapshot.data()
-    acc[title.toLowerCase()] = items
-    return acc
-  }, {})
-  return categoryMap
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((doc) => doc.data());
 };
 
 //  createUserDocumentFromAuth -> async function receive userAuth object
